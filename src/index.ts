4ac3efe37a3cbffdfd26f24a34cd303916abc9ab
Pria Admaja <priaadmaja@gmail.com>
2026-01-28 20:11:02 +0700

    Add cors

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,13 @@ import { Hono } from "hono";
 import { serve } from "@hono/node-server";
 import routes from "./routes/index.js";
 import { ContextWithPrisma } from "./types/app.js";
+import { cors } from "hono/cors";
+import { prettyJSON } from "hono/pretty-json";
 
 const app = new Hono<ContextWithPrisma>();
+
+app.use(prettyJSON());
+app.use("/*", cors());
 
 app.route("/", routes);
 
